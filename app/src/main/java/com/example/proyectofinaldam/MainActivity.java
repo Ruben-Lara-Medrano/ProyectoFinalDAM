ea7b2144468d1b28cd5e8be5a29fdd64bd4639fc
Ruben Lara <rlaram01@educantabria.es>
2020-10-31 18:12:24 +0100

    Creacion de menu con actions buttons, creacion, configuracion del xml de strings en ingles(terminado) y terminando creacion del perfil con imagen de perfil y opcion de tomar foto de  perfil.

--- app/src/main/java/com/example/proyectofinaldam/MainActivity.java
+++ app/src/main/java/com/example/proyectofinaldam/MainActivity.java
@@ -1,19 +1,91 @@
 package com.example.proyectofinaldam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class MainActivity extends AppCompatActivity {
+    private ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "OnCreate", Toast.LENGTH_LONG).show();
+        img=(ImageView)findViewById(R.id.imageView);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1000);
+        }
+    }
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void tomarFoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            img.setImageBitmap(imageBitmap);
+        }
     }
     @Override
     protected void onStart() {
@@ -50,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         //Carpeta res - menu - overflow y pasamos el objeto menu
         getMenuInflater().inflate(R.menu.overflow, menu);
+        getMenuInflater().inflate(R.menu.menuacciones, menu);
         return true;
     }
 
@@ -57,14 +130,23 @@ public class MainActivity extends AppCompatActivity {
         int id= item.getItemId();
 
         // Id de cada opcion del menu
-        if(id == R.id.menuPrincipal){
+        if(id == R.id.paginaPrincipal){
             Toast.makeText(this, "menuPrincipal", Toast.LENGTH_SHORT).show();
         }else if(id == R.id.registro){
             Toast.makeText(this, "registro", Toast.LENGTH_SHORT).show();
         }else if(id == R.id.paginaPrincipal){
             Toast.makeText(this, "paginaPrincipal", Toast.LENGTH_SHORT).show();
         }
+        else if(id == R.id.compartir){
+            Toast.makeText(this, "Compartir", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if(id == R.id.buscar){
+            Toast.makeText(this, "Buscar", Toast.LENGTH_SHORT).show();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
--- app/src/main/java/com/example/proyectofinaldam/perfil.java
+++ app/src/main/java/com/example/proyectofinaldam/perfil.java
@@ -1,8 +1,13 @@
 package com.example.proyectofinaldam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class perfil extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class perfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+
+
     }
+
+
 }
